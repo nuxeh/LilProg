f17b481cc5f13d3da123f05f59d012792f80c328
Ed Cragg <ed.cragg@eipi.xyz>
2023-02-28 17:11:05 +0000

    No field?

--- include/LilProg.h
+++ include/LilProg.h
@@ -8,13 +8,11 @@ template <class T>
 class LilProg {
   LilProg(T& l, style& s) : lcd(l), st(s) {}
   void draw(uint8_t x, uint8_t y, uint8_t w, uint8_t pc);
-
+  style& st;
+  T& lcd;
 private:
   void buildCharacter(mask *p, mask *n, uint8_t fill, uint8_t addr);
   void printChar(uint8_t w, uint8_t n, uint8_t fill) {
-
-  style& st;
-  T& lcd;
 };
 
 #endif
