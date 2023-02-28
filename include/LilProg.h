77a8dfe79f065aefe918e513d633ea98e664489b
Ed Cragg <ed.cragg@eipi.xyz>
2023-02-28 22:18:24 +0000

    Fixup again

--- include/LilProg.h
+++ include/LilProg.h
@@ -105,11 +105,11 @@ void LilProg<T>::draw(uint8_t x, uint8_t y, uint8_t w, uint8_t pc) {
 
   for (uint8_t i=0; i<w; i++) {
     if (i <= filledBlocks) {
-      printChar(w, i, 4);
+      printChar(i, 4);
     } else if (i == filledBlocks + 1 && haveTransitionBlock) {
-      printChar(w, i, transitionBlockFill);
+      printChar(i, transitionBlockFill);
     } else {
-      printChar(w, i, 0);
+      printChar(i, 0);
     }
   }
 }
