5788a554307ff6ce67999960cd707add82556f09
Ed Cragg <ed.cragg@eipi.xyz>
2023-02-28 19:05:01 +0000

    ...

--- include/LilProg.h
+++ include/LilProg.h
@@ -10,11 +10,13 @@ class LilProg {
 public:
   LilProg(T& l, const style& s) : lcd(l), st(s) {}
   void draw(uint8_t x, uint8_t y, uint8_t w, uint8_t pc);
-  const style& st;
-  T& lcd;
+
 private:
   void buildCharacter(mask *p, mask *n, uint8_t fill, uint8_t addr);
   void printChar(uint8_t w, uint8_t n, uint8_t fill);
+
+  const style& st;
+  T& lcd;
 };
 
 #endif
