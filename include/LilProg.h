c269fc3ada27c8a12f7f2e0d51fab0ad53978364
Ed Cragg <ed.cragg@eipi.xyz>
2023-02-28 18:55:06 +0000

    ...

--- include/LilProg.h
+++ include/LilProg.h
@@ -8,7 +8,7 @@ template <class T>
 class LilProg {
   LilProg(T& l, const style& s) : lcd(l), st(s) {}
   void draw(uint8_t x, uint8_t y, uint8_t w, uint8_t pc);
-  style& st;
+  const style& st;
   T& lcd;
 private:
   void buildCharacter(mask *p, mask *n, uint8_t fill, uint8_t addr);
