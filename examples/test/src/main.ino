c87536561887f94bafa4331603135af9c925c570
Ed Cragg <ed.cragg@eipi.xyz>
2023-02-28 21:32:13 +0000

    Reinstantiate full example

--- examples/test/src/main.ino
+++ examples/test/src/main.ino
@@ -5,7 +5,7 @@
 #include <styleRounded.h>
 
 LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POSITIVE);
-LilProg<LiquidCrystal_I2C> progress(lcd, rounded);
+LilProg<LiquidCrystal_I2C> prog(lcd, rounded);
 
 void setup() {
   Serial.begin(115200);
@@ -17,7 +17,7 @@ uint8_t pc = 0;
 
 void loop() {
   uint32_t t1 = micros();
-  //progress.draw(0, 0, 16, pc);
+  prog.draw(0, 0, 16, pc);
   Serial.print(micros() - t1);
   pc = (pc + 1) % 100;
   delay(100);
