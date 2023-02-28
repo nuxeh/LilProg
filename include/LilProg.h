635c28d9e831c813d15de988fc67f47aaa3ce50f
Ed Cragg <ed.cragg@eipi.xyz>
2023-02-28 22:04:06 +0000

    TEST: more debug

--- include/LilProg.h
+++ include/LilProg.h
@@ -53,18 +53,22 @@ void LilProg<T>::buildCharacter(const mask *n, const mask *p, uint8_t fill, uint
 
 template <class T>
 void LilProg<T>::printChar(uint8_t w, uint8_t n, uint8_t fill) {
+  Serial.printiln("A");
   if (n == 0) {
     buildCharacter(&st.maskLN, &st.maskLP, fill, 0);
     lcd.print("\10");
     lcd.print("<");
+    Serial.printiln("B");
   } else if (n == w) {
     buildCharacter(&st.maskRN, &st.maskRP, fill, 0);
     lcd.print("\10");
     lcd.print(">");
+    Serial.printiln("C");
   } else {
     buildCharacter(&st.maskMN, &st.maskMP, fill, 0);
     lcd.print("\10");
     lcd.print("=");
+    Serial.printiln("D");
   }
 }
 
