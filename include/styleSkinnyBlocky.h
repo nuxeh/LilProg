7cf9f89710864f416e5571b0fd77de89f216d6b9
Ed Cragg <ed.cragg@eipi.xyz>
2023-03-02 19:09:45 +0000

    styles: add Skinny and SkinnyBlocky styles

---
+++ include/styleSkinnyBlocky.h
@@ -0,0 +1,59 @@
+#ifndef __STYLE_SKINNYBLOCKY_H__
+#define __STYLE_SKINNYBLOCKY_H__
+
+#include "style.h"
+
+const style PROG_DECORATOR skinnyblocky = {
+  .maskLeft = {
+    .neg = {
+      0b00000000,
+      0b00000000,
+      0b00000000,
+      0b00011000,
+      0b00011000
+    },
+    .pos = {
+      0b00000000,
+      0b00111100,
+      0b01000010,
+      0b01000010,
+      0b01000010
+    },
+  },
+  .maskRight = {
+    .neg = {
+      0b00011000,
+      0b00011000,
+      0b00000000,
+      0b00000000,
+      0b00000000
+    },
+    .pos = {
+      0b01000010,
+      0b01000010,
+      0b01000010,
+      0b00111100,
+      0b00000000
+    },
+  },
+  .maskMid = {
+    .neg = {
+      0b00011000,
+      0b00011000,
+      0b00000000,
+      0b00011000,
+      0b00011000
+    },
+    .pos = {
+      0b01000010,
+      0b01000010,
+      0b01000010,
+      0b01000010,
+      0b01000010
+    },
+  },
+  .offsetLeft = 3,
+  .offsetRight = 3
+};
+
+#endif
