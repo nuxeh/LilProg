7a701ae7baf5deeb28cefc2732ce595c87ecf750
Ed Cragg <ed.cragg@eipi.xyz>
2023-03-02 13:54:05 +0000

    Complete smooth bar example

--- examples/SmoothBar/src/main.ino
+++ examples/SmoothBar/src/main.ino
@@ -1,21 +1,16 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-#include <LilProgTextMode.h>
+#include <LilProgSmoothBar.h>
 
 LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POSITIVE);
-LilProgTextMode<LiquidCrystal_I2C> prog(lcd);
+LilProgSmoothBar<LiquidCrystal_I2C> prog(lcd);
 
 void setup() {
   Serial.begin(115200);
-  #define VCC_LCD_EN 2
-  #define VCC_RUN_EN A3
-  pinMode(VCC_RUN_EN, OUTPUT);
-  pinMode(VCC_LCD_EN, OUTPUT);
-  digitalWrite(VCC_RUN_EN, LOW);
-  digitalWrite(VCC_LCD_EN, LOW);
   Wire.begin();
   lcd.begin();
+  prog.begin();
 }
 
 uint8_t pc = 0;
