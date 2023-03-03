a81fe354327181f1769d0822b48e2d3dbc0a2c52
Ed Cragg <ed.cragg@eipi.xyz>
2023-03-03 10:17:43 +0000

    scripts: generate all styles header in generateAll

--- scripts/generateAll.sh
+++ scripts/generateAll.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
+ALLSTYLES="../include/LilProgAllStyles.h"
+rm -f $ALLSTYLES
+echo "#ifndef __LILPROG_ALL_STYLES_H_" >> $ALLSTYLES
+echo "#define __LILPROG_ALL_STYLES_H_" >> $ALLSTYLES
+echo >> $ALLSTYLES
+
 for f in ../spec/*.txt; do
   name=$(basename "$f" ".txt")
   cat "$f" | awk -f spec2bytes.awk -v styleName="$name"\
     > ../include/style"$name".h
+  echo "#include \"style"$name".h\"" >> $ALLSTYLES
 done	
+
+echo >> $ALLSTYLES
+echo "#endif" >> $ALLSTYLES
