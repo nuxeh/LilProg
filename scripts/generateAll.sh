17054c5e6b1f95495fd4f9ace8a724cc3cf25e3a
Ed Cragg <ed.cragg@eipi.xyz>
2023-03-03 10:51:31 +0000

    scripts: generateAll script adds files to git

--- scripts/generateAll.sh
+++ scripts/generateAll.sh
@@ -7,11 +7,15 @@ echo "#define __LILPROG_ALL_STYLES_H_" >> $ALLSTYLES
 echo >> $ALLSTYLES
 
 for f in ../spec/*.txt; do
+  git add "$f"
   name=$(basename "$f" ".txt")
+  output="../include/style${name}.h"
   cat "$f" | awk -f spec2bytes.awk -v styleName="$name"\
-    > ../include/style"$name".h
+    > "$output"
+  git add "$output"
   echo "#include \"style"$name".h\"" >> $ALLSTYLES
 done	
 
 echo >> $ALLSTYLES
 echo "#endif" >> $ALLSTYLES
+git add $ALLSTYLES
